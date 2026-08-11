d4f232bc3341c61fce4330901dc1eb60fcce48cd
dadmov <dedsecmovies420@gmail.com>
2026-08-11 20:22:28 +0530

    add capacitor

---
+++ capacitor.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@capacitor/cli').CapacitorConfig} */
+const config = {
+  appId: 'com.galpotori.app',
+  appName: 'Galpotori',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+module.exports = config;
